1d9f48e89ae70e56a112f744dfce9846f71839cb
datapotomus <matthew.therault@gmail.com>
2021-08-18 21:40:47 -0400

    Got a working loop for the conjecture.

--- index.js
+++ index.js
@@ -1,5 +1,13 @@
 // Javascript Algorithm
 let nb = 0;
+let numList=[];
+const args = process.argv.slice(2)
+let endNum = args[0] || 2
+let topNumber = {
+  startingNum: 1,
+  iterations: 1,
+  numList: []
+}
 
 function step(n) {
   if (n%2 == 0) return n/2;
@@ -9,18 +17,31 @@ function step(n) {
 function collatz(n) {
   nb = 1;
   while (n != 1) {
+      numList.push(n)
       // console.log(n)
       n = step(n);
       nb++;
   }
-  return nb;
+  return [nb, numList];
 }
 
 // collatz(10090077777777723234234234324234234223423423432432);
 
-let endNum = 10;
 
-for (let index = 1; index < endNum; index++) {
+
+for (let index = 1; index <= endNum; index++) {
+
   collatz(index);
+
+  if (nb > topNumber.iterations) {
+    topNumber.startingNum = index;
+    topNumber.iterations = nb;
+    topNumber.numList = numList;
+  }
   console.log(`Starting Number: ${index} Iterations: ${nb}`);
+  numList=[];
 }
+
+console.log(`
+            
+Top Number is ${topNumber.startingNum} Top Iterations: ${topNumber.iterations} Number List ${topNumber.numList}`)
